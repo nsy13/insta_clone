efd9b4571d233dd113d5216f01e6f33e5d68b808
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-07-25 16:09:30 +0900

    add concern

---
+++ app/controllers/concerns/common_actions.rb
@@ -0,0 +1,17 @@
+require 'active_support/concern'
+
+module CommonActions
+  extend ActiveSupport::Concern
+
+  included do
+    helper_method :current_user, :logged_in?, :correct_user?
+  end
+
+  def logged_in_user
+    unless user_signed_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to login_path
+    end
+  end
+
+end
