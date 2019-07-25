468083aa4c99a57636a90565f900c38fc76519a2
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-07-25 16:19:12 +0900

    add some method to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,33 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "投稿しました"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def index
+    @posts = Post.page(params[:page]).per(60)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.delete
+    flash[:success] = "削除しました"
+    redirect_to root_url
   end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
 end
