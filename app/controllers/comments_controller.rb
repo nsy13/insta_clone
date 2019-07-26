f54dceeabece8f1b84034fc6e11a26cea49580eb
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-07-26 13:25:07 +0900

    improve comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,20 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      flash[:success] = "コメントを投稿しました"
+      redirect_to post_path
+    else
+      render 'posts/show'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
