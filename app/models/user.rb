3ab78a65dd660ea49b8454c1cb586c8b4da02e4f
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-07-29 22:13:44 +0900

    add user profile image from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.user_name = auth.info.name
+      user.image = auth.info.image
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_101059) do
+ActiveRecord::Schema.define(version: 2019_07_29_130512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_101059) do
     t.string "full_name"
     t.string "uid"
     t.string "provider"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
