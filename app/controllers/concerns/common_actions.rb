d880f465b77edd2097a83d704ec6c886baaef132
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-07-29 14:36:13 +0900

    edit common action

--- app/controllers/concerns/common_actions.rb
+++ app/controllers/concerns/common_actions.rb
@@ -4,8 +4,7 @@ module CommonActions
   extend ActiveSupport::Concern
 
   included do
-    helper_method :logged_in_user, :already_liked
-  end
+    helper_method :logged_in_user
 
   def logged_in_user
     unless user_signed_in?
@@ -13,10 +12,4 @@ module CommonActions
       redirect_to login_path
     end
   end
-
-  def already_liked
-    @post = Post.find(params[:id])
-    return true if Like.where(post_id: @post.id)
-  end
-
 end
