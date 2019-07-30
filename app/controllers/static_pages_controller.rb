5298fc53557a8e392c0f5f26a256503e70e9449d
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-07-31 01:35:24 +0900

    edit view of feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @feed_items = current_user.feed.page(params[:page]).per(30)
+    if user_signed_in?
+      @feed_items = current_user.feed.page(params[:page]).per(30)
+    end
   end
 end
