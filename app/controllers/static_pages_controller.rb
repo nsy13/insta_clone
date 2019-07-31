056a81b9f4ee5db28aff06c0e51141ea1efdfc6a
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-07-31 16:51:06 +0900

    refactored feed on top page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    @like = Like.new
+    @comment = Comment.new
     if user_signed_in?
       @feed_items = current_user.feed.page(params[:page]).per(30)
     end
