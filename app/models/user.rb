62a6236a2c37196d48e390eb02ab6ba13f96c0e3
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-08-05 14:22:45 +0900

    edit some files to correspond to style rule with rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,19 @@ class User < ApplicationRecord
   include Gravtastic
   gravtastic
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i(facebook)
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :notifications, dependent: :destroy
-  has_many :active_relationships, class_name: "Relationship",
-            foreign_key: "follower_id",
-            dependent: :destroy
-  has_many :passive_relationships, class_name: "Relationship",
-            foreign_key: "followed_id",
-            dependent: :destroy
+  has_many :active_relationships,
+           class_name: "Relationship",
+           foreign_key: "follower_id",
+           dependent: :destroy
+  has_many :passive_relationships,
+           class_name: "Relationship",
+           foreign_key: "followed_id",
+           dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   validates :user_name, presence: true
